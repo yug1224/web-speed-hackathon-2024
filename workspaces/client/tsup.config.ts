e6fb52c836452e3c1faf2af1a3adfa5326573d8e
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2024-03-23 23:28:54 +0900

    update

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -1,40 +1,48 @@
-import fs from 'node:fs';
-import path from 'node:path';
+import fs from "node:fs";
+import path from "node:path";
 
-import { pnpmWorkspaceRoot as findWorkspaceDir } from '@node-kit/pnpm-workspace-root';
-import { polyfillNode } from 'esbuild-plugin-polyfill-node';
-import findPackageDir from 'pkg-dir';
-import { defineConfig } from 'tsup';
-import type { Options } from 'tsup';
+import { pnpmWorkspaceRoot as findWorkspaceDir } from "@node-kit/pnpm-workspace-root";
+import { polyfillNode } from "esbuild-plugin-polyfill-node";
+import findPackageDir from "pkg-dir";
+import { defineConfig } from "tsup";
+import type { Options } from "tsup";
 
 export default defineConfig(async (): Promise<Options[]> => {
   const PACKAGE_DIR = (await findPackageDir(process.cwd()))!;
   const WORKSPACE_DIR = (await findWorkspaceDir(process.cwd()))!;
 
-  const OUTPUT_DIR = path.resolve(PACKAGE_DIR, './dist');
+  const OUTPUT_DIR = path.resolve(PACKAGE_DIR, "./dist");
 
-  const SEED_IMAGE_DIR = path.resolve(WORKSPACE_DIR, './workspaces/server/seeds/images');
-  const IMAGE_PATH_LIST = fs.readdirSync(SEED_IMAGE_DIR).map((file) => `/images/${file}`);
+  const SEED_IMAGE_DIR = path.resolve(
+    WORKSPACE_DIR,
+    "./workspaces/server/seeds/images",
+  );
+  const IMAGE_PATH_LIST = fs.readdirSync(SEED_IMAGE_DIR).map((file) =>
+    `/images/${file}`
+  );
 
   return [
     {
       bundle: true,
       clean: true,
       entry: {
-        client: path.resolve(PACKAGE_DIR, './src/index.tsx'),
-        serviceworker: path.resolve(PACKAGE_DIR, './src/serviceworker/index.ts'),
+        client: path.resolve(PACKAGE_DIR, "./src/index.tsx"),
+        serviceworker: path.resolve(
+          PACKAGE_DIR,
+          "./src/serviceworker/index.ts",
+        ),
       },
       env: {
-        API_URL: '',
-        NODE_ENV: process.env['NODE_ENV'] || 'development',
-        PATH_LIST: IMAGE_PATH_LIST.join(',') || '',
+        API_URL: "",
+        NODE_ENV: process.env["NODE_ENV"] || "development",
+        PATH_LIST: IMAGE_PATH_LIST.join(",") || "",
       },
       esbuildOptions(options) {
         options.define = {
           ...options.define,
-          global: 'globalThis',
+          global: "globalThis",
         };
-        options.publicPath = '/';
+        options.publicPath = "/";
       },
       esbuildPlugins: [
         polyfillNode({
@@ -48,19 +56,19 @@ export default defineConfig(async (): Promise<Options[]> => {
           },
         }),
       ],
-      format: 'iife',
+      format: "iife",
       loader: {
-        '.json?file': 'file',
-        '.wasm': 'binary',
+        ".json?file": "file",
+        ".wasm": "binary",
       },
       metafile: true,
       minify: true,
       outDir: OUTPUT_DIR,
-      platform: 'browser',
+      platform: "browser",
       shims: true,
-      sourcemap: 'inline',
+      sourcemap: true,
       splitting: false,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
+      target: ["chrome58", "firefox57", "safari11", "edge18"],
       treeshake: false,
     },
   ];
