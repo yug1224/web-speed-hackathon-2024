356adbe8368b1fd2e10aa1a7b0bdf6a4a9120293
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2024-03-24 03:10:22 +0900

    update

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
-import { logger } from "hono/logger";
+// import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
 
 import { cacheControlMiddleware } from "../middlewares/cacheControlMiddleware";
@@ -14,7 +14,7 @@ import { ssrApp } from "./ssr";
 import { staticApp } from "./static";
 
 const app = new Hono();
-app.use(logger());
+// app.use(logger());
 app.use(secureHeaders());
 app.use(
   cors({
